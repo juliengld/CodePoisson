ac76d3855a0dfc27644af5c7cf7d492f3054627e
WiziPowi <george.nuttall175@gmail.com>
2025-12-10 11:43:15 +0100

    Update AsservProfond.cpp

--- AsservProfond.cpp
+++ AsservProfond.cpp
@@ -1,4 +1,14 @@
 #include CommandMotor.h
 #include Capteurs.h
 
-  
+  void AsservProfond::setProfondeurVoulue(float ProfMetres)
+{
+
+    
+    if (ProfMetres < 0.0f)   ProfMetres = 0.0f;
+    if (ProfMetres > 180.0f) ProfMetres = 180.0f;
+
+    servo.write(angleDeg);
+}
+
+setServoAngle
