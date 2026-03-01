df1bc58521c4df6eb359a22645d9282d7add7a64
Lea-Manu <manulea34@gmail.com>
2026-03-01 14:29:29 +0100

    Update Controller.h

--- Controller.h
+++ Controller.h
@@ -8,16 +8,14 @@
 #define CONTROLLER_HPP
 
 #include <Arduino.h>
-#include "CommandMotor.h"   // ou CommandMotor.hpp selon fichier réel
+#include "CommandMotor.h" 
 #include "StateMachine.h"
 
-// ----- Modes -----
 enum class ControlMode {
     MANUAL,
     AUTONOMOUS
 };
 
-// ----- Commandes disponibles -----
 enum class CommandType {
     NONE,
     FORWARD,
@@ -31,16 +29,13 @@ enum class CommandType {
 class Controller
 {
 public:
-    // Le controller a besoin d’un accès au moteur :
     Controller(CommandMotor& motor, StateMachine& stateMachine);
 
     void begin();
     void update();
 
-    // Entrée “clavier” (z, q, d, s, a, e, f)
     void onKey(char key);
 
-    // Entrée générique (Wifi ou StateMachine)
     void onCommand(CommandType cmd);
 
     ControlMode mode() const { return _mode; }
@@ -48,7 +43,6 @@ public:
 private:
     void applyManualCommand(CommandType cmd);
 
-    // Actions bas niveau :
     void goStraight(float speed);
     void turnLeft(float speed);
     void turnRight(float speed);
@@ -58,7 +52,7 @@ private:
     void exitAutonomousMode();
 
 private:
-    CommandMotor& _motor;   // référence vers ton moteur
+    CommandMotor& _motor; 
 
     ControlMode _mode;
     CommandType _lastManualCmd;
