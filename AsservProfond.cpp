b93d944ecfe425a14f127e0e029bfe73365aa08a
Lea-Manu <manulea34@gmail.com>
2026-03-01 14:34:03 +0100

    Update AsservProfond.cpp

--- AsservProfond.cpp
+++ AsservProfond.cpp
@@ -4,11 +4,8 @@ AsservProfond::AsservProfond(CommandMotor* motorPtr, Capteurs* capteursPtr) {
     _motor = motorPtr;
     _capteurs = capteursPtr;
     
-    // Gain: A ajuster. 
-    // Comme la course est grande (360°), on peut augmenter le gain par rapport à un servo 180°.
     _gainProportionnel = 40.0f; 
     
-    // Neutre: Milieu de la course du servo 360
     _angleNeutre = 180.0f;       
 }
 
@@ -30,28 +27,21 @@ void AsservProfond::setServoAngle(float angle) {
 
 void AsservProfond::setProfondeurVoulue(float ProfMetres)
 {
-    // 1. Sécurité borne consigne
     if (ProfMetres < 0.0f) ProfMetres = 0.0f;
     if (ProfMetres > _profondeurSecurite) ProfMetres = _profondeurSecurite;
 
-    // 2. Lecture
     float ProfActuelle = getProfondeur(); 
 
-    // 3. Calcul Erreur 
-    // Si ProfActuelle < Cible (on est trop haut) -> Erreur > 0 -> On doit remplir (augmenter angle)
     float erreur = ProfMetres - ProfActuelle;
 
-    // 4. Commande P
     float commandeAngle = _angleNeutre + (erreur * _gainProportionnel);
 
-    // 5. Saturation 0-360
     if (commandeAngle < _angleMin) commandeAngle = _angleMin;
     if (commandeAngle > _angleMax) commandeAngle = _angleMax;
 
-    // 6. Envoi
     setServoAngle(commandeAngle);
     
-    // Debug optionnel
+    // Debug 
     /*
     Serial.print("Cible: "); Serial.print(ProfMetres);
     Serial.print(" Actuel: "); Serial.print(ProfActuelle);
